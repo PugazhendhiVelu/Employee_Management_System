848df270b9c606ca081da226751550a4ca608b78
PUGAZHENDHI V <126157273+PugazhendhiVelu@users.noreply.github.com>
2024-09-02 20:53:33 +0530

    Delete backend directory

--- backend/routes/addproject.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const path = require('path')
-const {createProject} = require(path.join(__dirname,'..','controller','AddProject'));
-
-router.route('/new/').post(createProject);
-
-module.exports = router;
--- backend/routes/editproject.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const path = require('path')
-const {editProject} = require(path.join(__dirname,'..','controller','EditProject'));
-
-router.route('/id/:projectId').put(editProject);
-
-module.exports = router;
